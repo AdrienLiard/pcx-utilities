e8f40bfd4d40809a97860295b20aebf4ca0c0216
AdrienLiard <liard.adrien@gmail.com>
2020-12-16 14:13:56 +0100

    format

--- kairntech.py
+++ kairntech.py
@@ -2,10 +2,6 @@ import requests
 import json
 
 
-
-
-    
-
 class KairntechClient():
 
     def __get_headers(self, token=None, content_type="application/json"):
@@ -44,21 +40,22 @@ class KairntechClient():
         else:
             return []
 
+
 class Annotator():
 
-    def __init__(self,client:KairntechClient, project_name:str, annotator_name:str):
+    def __init__(self, client: KairntechClient, project_name: str, annotator_name: str):
         self.project_name = project_name
         self.annotator_name = annotator_name
         self.client = client
 
     def annotate(self, document: str):
-            headers = self.client.__get_headers(self.client.token, "text/plain")
-            r = requests.post(
-                f"{self.client.api_url}projects/{self.project_name}/annotators/{self.annotator_name}/_annotate", headers=headers, data=document)
-            results = []
-            for c in r.json()["categories"]:
-                if "label" in c:
-                    results.append(c["label"])
-                else:
-                    results.append(c["labelName"])
-            return results
+        headers = self.client.__get_headers(self.client.token, "text/plain")
+        r = requests.post(
+            f"{self.client.api_url}projects/{self.project_name}/annotators/{self.annotator_name}/_annotate", headers=headers, data=document)
+        results = []
+        for c in r.json()["categories"]:
+            if "label" in c:
+                results.append(c["label"])
+            else:
+                results.append(c["labelName"])
+        return results
