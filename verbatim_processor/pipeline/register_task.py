69adbf83b532e2f374f7c3c4a66e5a5cab596325
AdrienLiard <liard.adrien@gmail.com>
2020-12-20 09:29:50 +0100

    register task delete

--- verbatim_processor/pipeline/register_task.py
+++
@@ -1,9 +0,0 @@
-from .task import Task
-from typing import List
-
-class TaskItem():
-
-  def __init__(self, name:str, task:Task, dependencies: List[str]):
-      self.name = name
-      self.task = Task
-      self.dependencies = dependencies
